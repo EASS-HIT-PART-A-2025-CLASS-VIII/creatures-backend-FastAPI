09848e9a3be56688fc3fac7787c8fe39266bd55f
Liad Vardi <56168537+LiadVardi@users.noreply.github.com>
2025-12-21 08:37:01 +0200

    Change mythology field type from int to str
    Correcting a mistake

--- models.py
+++ models.py
@@ -6,6 +6,6 @@ from sqlmodel import SQLModel, Field
 class Creature(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     name: str
-    mythology: int
+    mythology: str
     creature_type: str
     danger_level: int
